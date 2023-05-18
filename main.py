bbbcf68558a28d2a155b947a3550c58dc9f59fa4
vadim <ivadimn@mail.ru>
2023-05-18 17:03:16 +0300

    First commit 18.05.2023

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+from application import Application
+from ui.main_window import MainWindow
+
+if __name__ == '__main__':
+    app = Application(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec())
+
+
+
