c955ff5200cb36f74c2edb951a7e205b4788e0fb
vadim <ivadimn@mail.ru>
2023-05-19 16:22:55 +0300

    commit 19.05.2023

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QMessageBox
+from PyQt6.QtCore import pyqtSlot
 from ui.main_menu import MainMenu
 
 
@@ -6,6 +7,20 @@ class MainWindow(QMainWindow):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.setGeometry(100, 100, 500, 250)
 
         main_menu = MainMenu(parent=self)
         self.setMenuBar(main_menu)
+
+        main_menu.about_qt.triggered.connect(self.about_qt)
+        main_menu.about.triggered.connect(self.about)
+
+    def about(self):
+        title = "Управление заданиями для учащихся"
+        text = ("Программа для управления задачами\n" +
+                "и заданиями для учащихся школ.")
+        QMessageBox.about(self, title, text)
+
+    @pyqtSlot()
+    def about_qt(self):
+        QMessageBox.aboutQt(self, "Управление заданиями для учащихся")
