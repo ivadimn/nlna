ca3f3d8763f9b9f97a252821845101a1901081fd
vadim <ivadimn@mail.ru>
2023-06-16 15:50:31 +0300

    restore project

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 db_params = {
     "host": "10.0.0.101",
     "port": 5432,
-    "user": "lna2admin",
-    "dbname": "lna2",
+    "user": "nlna_admin",
+    "dbname": "nlna",
     "password": "7922448",
 }
