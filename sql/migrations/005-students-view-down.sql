d6993485805ba19f58428c30e03ebdcf96caf33a
vadim <ivadimn@mail.ru>
2023-08-31 21:24:04 +0300

    create db migrations

--- sql/migrations/005-students-view-down.sql
+++ sql/migrations/005-students-view-down.sql
@@ -1,5 +1,6 @@
 START TRANSACTION ;
 
+DROP FUNCTION IF EXISTS new_student(text, text, text, text);
 DROP FUNCTION IF EXISTS new_teacher(text, text, text, text, text);
 DROP VIEW IF EXISTS v_student ;
 DROP VIEW IF EXISTS v_teacher ;
